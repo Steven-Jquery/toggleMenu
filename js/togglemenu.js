87aeb397687566f34ec4c481602a0702528f9e25
huynhtrongdai1985 <34928128+huynhtrongdai1985@users.noreply.github.com>
2018-11-15 09:58:53 +0700

    cm-toggle

--- js/togglemenu.js
+++ js/togglemenu.js
@@ -1,10 +1,23 @@
 var $ = jQuery.noConflict();
 $(document).ready(function(){
-	$('.list-accordion .item-accordion .title').click(function(){
-		var $this = $(this);
-		$this.parent().siblings('li').find('.content-accordion').slideUp(350);//close when click siblings item
-		$this.toggleClass('active');
-		$this.parent().siblings('li').find('.active').removeClass('active');
-		$this.next().slideToggle(350);
+	//open toggle menu
+	$('.icon-menu').click(function(e){
+		$('html, .navigation').addClass('fixed-body open');
+		$('.wrap-overlay').show();
 	});
+
+	//closed toggle menu
+	$('.icon-closed').click(function(){
+		$('html, .navigation').removeClass('fixed-body open');
+		$('.wrap-overlay').hide();
+	});
+
+});
+
+//closed toggle menu when click outsite
+$(document).click(function(e){
+	if(!$(e.target).closest('.navigation, .icon-menu').length){
+        $('html, .navigation').removeClass('fixed-body open');
+		$('.wrap-overlay').hide();
+    }
 });
